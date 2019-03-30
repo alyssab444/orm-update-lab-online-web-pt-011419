71459e48ffea1640a0e182909cd65312a5e340aa
Alyssa Brown <alyssa.brown@live.com>
2019-03-30 00:08:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,10 +32,11 @@ def self.create_table
    sql =  <<-SQL
       INSERT INTO students (name,grade) VALUES (?,?)
         SQL
-       
      DB[:conn].execute(sql,self.name,self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
+  
+  
    
 end 
 
