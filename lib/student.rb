c3e71398629014d9542b5578288a43ab0f5fb934
Alyssa Brown <alyssa.brown@live.com>
2019-03-30 00:52:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ end
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
     
-  def self.create
+  def self.create(name,grade)
     student = Student.new(name, grade)
     student.save
     student
